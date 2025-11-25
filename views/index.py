3d0bfe77cb52fb2b3970e6f919c078bce0f14267
Hongpe <Hongpelembaw@gmail.com>
2025-11-25 20:58:17 +0530

    test1

--- views/index.py
+++ views/index.py
@@ -1,5 +1,6 @@
 import flet as ft
 from mypack import  misc
+import random
 
 def IndexView(page:ft.Page, params):
     def CreateAppBar():
@@ -17,11 +18,26 @@ def IndexView(page:ft.Page, params):
         )
         return app_bar
     def hint(e):
-        dlg =ft.AlertDialog(title=ft.Text("Sorry No Hint for You :-} "))
+        dlg =ft.AlertDialog(title=ft.Text(hint))
         page.open(dlg)
     def restart_clicked(e):
-         dlg = ft.AlertDialog(title=ft.Text("New Game!"))
-         page.open(dlg)
+
+        nonlocal current_row, Answer,hint
+        current_row =0
+        Guess.disabled=False
+        Guess.value=""
+        Answer,hint = random.choice(word_list)
+        message.value="Guess the Word"
+
+        for r in range(rows):
+            for c in range(cols):
+                box_txt[r][c].value = ""
+        print("Answer:",Answer)
+        page.update()
+        dlg = ft.AlertDialog(title=ft.Text("New Game started!"))
+        page.open(dlg)
+
+
 
     def btn_question1_clicked(e):
         page.go("/question/1")
@@ -41,34 +57,105 @@ def IndexView(page:ft.Page, params):
 
     appbar = CreateAppBar()
 
-    def build_board(rows=5, cols=5):
+    def build_board(rows, cols):
         board = ft.Column()
+
+        box_txt = []
 
         for _ in range(rows):
             row = ft.Row(alignment=ft.MainAxisAlignment.CENTER)
+            row_box_txt = []
 
             for _ in range(cols):
-                txt = ft.Text("A", color=ft.Colors.WHITE, size=28)
+                txt = ft.Text(
+                    value="",
+                    color= ft.Colors.WHITE,
+                    size=28,
+                    weight=ft.FontWeight.BOLD,
+                    text_align=ft.TextAlign.CENTER,
+                )
                 row.controls.append(
                     ft.Container(
-                        width=45,
-                        height=45,
-                        bgcolor=ft.Colors.BLUE_GREY_900,
-                        border_radius=10,
-                        alignment=ft.alignment.center,
-                        content=txt
-
-                    )
+                    width=45,
+                    height=45,
+                    bgcolor=ft.Colors.BLUE_GREY_900,
+                    border_radius=10,
+                    alignment=ft.alignment.center,
+                    content=txt
                 )
-            board.controls.append(row)
-        return board
+                )
 
-    Guess = ft.TextField(label="Guess The Word",max_length=5,width=250)
+                row_box_txt.append(txt)
+
+            box_txt.append(row_box_txt)
+            board.controls.append(row)
+        return board,  box_txt
+
+
+
+    def check_guess(e):
+        nonlocal current_row, Answer
+
+        if current_row >= rows or Guess.disabled:
+            return
+
+        guess = Guess.value.strip().upper()
+
+        if len(guess) != cols or not guess.isalpha():
+            message.value = "Enter exactly 5 letters."
+            page.update()
+            return
+
+        for i, ch in enumerate(guess):
+            box_txt[current_row][i].value = ch
+
+        if guess == Answer:
+            message.value = f" You got it! The word was {Answer}."
+            message.color= ft.Colors.GREEN
+            Guess.disabled = True
+        else:
+            current_row += 1
+            if current_row >= rows:
+                message.value = f" Out of tries! The word was {Answer}."
+                Guess.disabled = True
+
+        Guess.value = ""
+        page.update()
+
+
+
+
+    Guess = ft.TextField(label="Type here", max_length=5, width=250, on_submit=check_guess)
+
+    message = ft.Text(
+        value="Guess the Word",)
+    page.update()
+
+
+
+
+    board,  box_txt = build_board(rows=5, cols=5)
+    current_row = 0
+    rows = 5
+    cols = 5
+
+
+
     wordle_words = misc.ReadCSV("data/wordle_words.txt")
+    word_list = [
+        (row[0].strip().upper(), row[1].strip())
+        for row in wordle_words
+    ]
+
+    Answer, hint = random.choice(word_list)
+
+
+
     print(wordle_words)
+    print("Answer:",Answer)
     page.views.append(ft.View(
         "/",
-        [appbar, build_board(),Guess ],
+        [appbar, board,Guess, message ],
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
 
     )
